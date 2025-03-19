4b13c9ca2d36f12c99ecc07f99e78034be7b5d89
Pavel <pavel@p-p.name>
2025-03-19 14:01:53 -0400

    + mDNS

--- wifi_manager.cpp
+++ wifi_manager.cpp
@@ -8,6 +8,7 @@
 #include "time_sync.h"
 #include "ota.h"
 #include <WiFi.h>
+#include <ESPmDNS.h>
 #include <Ticker.h>
 // #include <NetworkClient.h>
 // #include <WiFiAP.h>
@@ -47,9 +48,19 @@ void onWiFiEvent(WiFiEvent_t event) {
         case ARDUINO_EVENT_WIFI_STA_GOT_IP:
             logEvent("Connected to Wi-Fi: " + WiFi.localIP().toString());
             setIndicatorState(IndicatorState::NONE);
-            wifiScanner.detach();  // Останавливаем сканирование в STA режиме
-            startTimeSync(); // Запускаем синхронизацию времени при подключении к Wi-Fi
+            wifiScanner.detach();  // Останавливаем сканирование
+
+            // === ИНИЦИАЛИЗАЦИЯ mDNS ===
+            if (!MDNS.begin(String(config["host_name"]))) {  
+                logEvent("mDNS setup failed");
+            } else {
+                logEvent("mDNS started: " + String(config["host_name"]) + ".local");
+                MDNS.addService("http", "tcp", 80); // Регистрируем HTTP-сервис
+            }
+
             checkWiFiAndOTA();
+
+            // startTimeSync();
             break;
         
         case ARDUINO_EVENT_WIFI_STA_DISCONNECTED:
@@ -112,84 +123,14 @@ void onWiFiEvent(WiFiEvent_t event) {
 
 }
 
-// void setupWiFi() {
-//     logEvent("Wi-Fi: try connect to " + String(WIFI_SSID));
-//     WiFi.mode(WIFI_STA);  // Сначала пробуем подключение как клиент
-//     WiFi.begin(WIFI_SSID, WIFI_PASS);
-    
-//     setIndicatorState(IndicatorState::CONNECTING);
-    
-//     if (WiFi.waitForConnectResult() != WL_CONNECTED) {
-//         logEvent("Wi-Fi: connect to " + String(WIFI_SSID) + " filed, enabling AP mode");
-
-//         // WiFi.mode(WIFI_AP_STA);  // Включаем STA+AP одновременно
-//         WiFi.mode(WIFI_AP);
-//        if (WiFi.softAP(AP_SSID, AP_PASS)){
-//           setIndicatorState(IndicatorState::AP_MODE);
-//           logEvent("AP mode active: " + String(AP_SSID)+ ", IP: " + WiFi.softAPIP().toString());
-//         } else {
-//           setIndicatorState(IndicatorState::ERROR);
-//           logEvent("AP mode fail");
-//         }
-
-//     } else {
-//         setIndicatorState(IndicatorState::NONE);
-//         logEvent("Connected to Wi-Fi: " + WiFi.localIP().toString());
-//     }
-
-//     // Регистрируем обработчик событий Wi-Fi
-//     WiFi.onEvent(onWiFiEvent);
-// }
-
 // void handleWiFi() {
 //     static unsigned long lastCheck = 0;
 //     if (millis() - lastCheck > 10000) {  // Проверка каждые 10 сек
 //         lastCheck = millis();
-
 //         if (WiFi.status() != WL_CONNECTED) {
 //             logEvent("Wi-Fi lost, reconnecting...");
 //             WiFi.disconnect();
 //             WiFi.reconnect();
 //         }
-//     }
-// }
-
-// void onWiFiEvent(WiFiEvent_t event) {
-//     switch (event) {
-//         case ARDUINO_EVENT_WIFI_STA_DISCONNECTED:
-//             logEvent("Wi-Fi disconnected, trying to reconnect...");
-//             setIndicatorState(IndicatorState::CONNECTING);
-//             WiFi.reconnect();
-//             break;
-
-//         case ARDUINO_EVENT_WIFI_STA_CONNECTED:
-//             logEvent("Wi-Fi connected to AP");
-//             break;
-
-//         case ARDUINO_EVENT_WIFI_STA_GOT_IP:
-//             logEvent("Wi-Fi connected, IP: " + WiFi.localIP().toString());
-//             setIndicatorState(IndicatorState::NONE);
-//             break;
-
-//         case ARDUINO_EVENT_WIFI_AP_STACONNECTED:
-//             logEvent("Client connected to AP");
-//             break;
-
-//         case ARDUINO_EVENT_WIFI_AP_STADISCONNECTED:
-//             logEvent("Client disconnected from AP");
-//             break;
-//     }
-// }
-
-
-
-// void handleWiFi() {
-//     static unsigned long lastBlink = 0;
-//     if (WiFi.getMode() == WIFI_MODE_AP && millis() - lastBlink > 3000) {
-//         digitalWrite(STATUS_LED, HIGH);
-//         delay(100);
-//         digitalWrite(STATUS_LED, LOW);
-//         delay(100);
-//         lastBlink = millis();
 //     }
 // }
